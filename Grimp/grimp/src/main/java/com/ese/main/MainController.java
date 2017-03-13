ac0890449a71f3008d797da4639d7f05bf56ffdc
kimyh <kimyh@ab8f325c-dba2-854a-871e-2fa0d5d12768>
2017-03-13 09:41:12 +0000

    1. Axisj Grid에서JqGrid 형태로 변경  - 기존 등록,수정,삭제와 SelectBox만드는 것들은 없애버리고 오로지 Grid 역할만 충실하게 수행할 수 있게 변경함

--- Grimp/grimp/src/main/java/com/ese/main/MainController.java
+++ Grimp/grimp/src/main/java/com/ese/main/MainController.java
@@ -1,15 +1,8 @@
 package com.ese.main;
 
 import java.util.HashMap;
-
 import java.util.Locale;
 import java.util.Map;
-
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
-
-import net.sf.json.JSONObject;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
@@ -17,11 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import c.e.g.domain.Grivo;
 import c.e.g.grimp.Grimp;
-
-import com.ese.cmmn.CmmnUtil;
 import com.ese.config.resolverHandler.RequestMap.RequestMap;
 import com.ese.domain.Dvsn;
 import com.ese.main.service.MainService;
@@ -52,9 +42,8 @@ public class MainController {
 	@Autowired
 	MessageSource message;
 	
-	
 	/**
-	 * @Method Name : main
+	 * @Method Name : main_jq
 	 * @create Date : 2016. 3. 11.
 	 * @made by : "GOEDOKID"
 	 * @explain : 그리드 화면 조회
@@ -62,11 +51,11 @@ public class MainController {
 	 * @return : String
 	 */
 	@RequestMapping(value="/main")
-	public String main(
+	public String main_jq(
 			Model model,
 			Locale locale
 			) {
-		model.addAttribute("grimpHeader", grimp.buildGrimpHeader(Dvsn.class, message, locale));
+		model.addAttribute("grimpModel", grimp.buildGrimpHeader(Dvsn.class, message, locale));
 		return "main/main";
 	}
 	
@@ -97,7 +86,7 @@ public class MainController {
 	public @ResponseBody Grivo mrider(
 			RequestMap requestMap
 			) {
-		return mainService.getDvsnMap(requestMap.getMap());
+		return  mainService.getDvsnMap(requestMap.getMap());
 	}
 	
 	/**
@@ -110,15 +99,15 @@ public class MainController {
 	 */
 	@RequestMapping(value="/griderStr")
 	public @ResponseBody String griderStr(
-			@RequestParam("pageNo") String pageNo,
-			@RequestParam("pageSize") String pageSize
+			@RequestParam("page") String page,
+			@RequestParam("rows") String rows
 			) {
 		
 		Map<String, Object> paramMap = new HashMap<String, Object>();
-		paramMap.put("pageNo", pageNo);
-		paramMap.put("pageSize", pageSize);
+		paramMap.put("page", page);
+		paramMap.put("rows", rows);
 		
-		return grimp.buildGrimp(mainService.getDvsnDomain(paramMap));
+		return grimp.buildGrimpToStr(mainService.getDvsnDomain(paramMap));
 	}
 	
 	/**
@@ -131,116 +120,15 @@ public class MainController {
 	 */
 	@RequestMapping(value="/mriderStr")
 	public @ResponseBody String mriderStr(
-			@RequestParam("pageNo") String pageNo,
-			@RequestParam("pageSize") String pageSize
+			@RequestParam("page") String page,
+			@RequestParam("rows") String rows
 			) {
 		
 		Map<String, Object> paramMap = new HashMap<String, Object>();
-		paramMap.put("pageNo", pageNo);
-		paramMap.put("pageSize", pageSize);
+		paramMap.put("page", page);
+		paramMap.put("rows", rows);
 		
-		return grimp.buildGrimp(mainService.getDvsnMap(paramMap), Dvsn.class);
-	}
-	
-	/**
-	 * @Method Name : template
-	 * @create Date : 2016. 3. 11.
-	 * @made by : "GOEDOKID"
-	 * @explain : 템플릿 그리드
-	 * @param : 
-	 * @return : String
-	 */
-	@RequestMapping(value="/template")
-	public String template() {
-		return "angur/axGrid_temp";
-	}
-	
-	/**
-	 * @Method Name : single
-	 * @create Date : 2016. 3. 11.
-	 * @made by : "GOEDOKID"
-	 * @explain : 테스트용 싱글 그리드 
-	 * @param : 
-	 * @return : String
-	 */
-	@RequestMapping(value="/single")
-	public String single(
-			HttpServletRequest request,
-			Locale locale,
-			Model model
-			) {
-		String grimpHeader = grimp.buildGrimpHeader(Dvsn.class, message, locale);
-		model.addAttribute("grimpHeader", grimpHeader);
-		return "angur/axGrid_single";
-	}
-	
-	/**
-	 * @Method Name : getDvsnCd
-	 * @create Date : 2016. 3. 11.
-	 * @made by : "GOEDOKID"
-	 * @explain : Grid 내부의 SelectBox 조회
-	 * @param : 
-	 * @return : String
-	 */
-	@RequestMapping(value="/getDvsnCd")
-	public @ResponseBody String getDvsnCd() {
-		return grimp.buildSelectString(mainService.getDvsnCd());
-	}
-	
-	/**
-	 * @Method Name : saveGrid
-	 * @create Date : 2016. 3. 11.
-	 * @made by : "GOEDOKID"
-	 * @explain : 그리드 저장 및 수정
-	 * @param : 
-	 * @return : String
-	 */
-	@RequestMapping(value="/saveGrid")
-	public @ResponseBody String saveGrid(
-			HttpServletRequest request
-			) {
-
-		Map<String, Object> paramMap = CmmnUtil.getParameterMap(request); 
-
-		String type = paramMap.get("requestType").toString();
-		int result = 0;
-		JSONObject json = new JSONObject();
-		
-		if(type.equals("new")) {
-			paramMap.put("ordr", 1);
-			result = mainService.addDvsnCd(paramMap);	
-		} else {
-			result = mainService.updateDvsnCd(paramMap);
-		}
-		json.put("isNew", type);
-		
-		if(result > 0)
-			json.put("isDone", true);
-		else
-			json.put("isDone", false);
-		
-		return json.toString();
-	}
-	
-	/**
-	 * @Method Name : deleteDvsn
-	 * @create Date : 2016. 3. 11.
-	 * @made by : "GOEDOKID"
-	 * @explain : 분류코드 삭제 
-	 * @param : String dvsnCd
-	 * @return : String
-	 */
-	@RequestMapping(value="/deleteDvsn")
-	public @ResponseBody String deleteDvsn(
-			@RequestParam(name="dvsnCd") String dvsnCd
-			) {
-	
-		int result = mainService.deleteDvsn(dvsnCd);
-		
-		if(result > 0)
-			return "true";
-		else
-			return "false";
+		return grimp.buildGrimpToStr(mainService.getDvsnMap(paramMap), Dvsn.class);
 	}
 	
 	/**
@@ -251,18 +139,18 @@ public class MainController {
 	 * @param : 
 	 * @return : String
 	 */
-	@RequestMapping(value="getDvsnList")
+	@RequestMapping(value="/getDvsnList")
 	public @ResponseBody String getDvsnList(
-			@RequestParam("pageNo") String pageNo,
-			@RequestParam("pageSize") String pageSize
+			@RequestParam("page") String page,
+			@RequestParam("rows") String rows
 			) {
 
 		Map<String, Object> paramMap = new HashMap<String, Object>();
-		paramMap.put("pageNo", pageNo);
-		paramMap.put("pageSize", pageSize);
+		paramMap.put("page", page);
+		paramMap.put("rows", rows);
 		
 		Grivo grivo =  mainService.getDvsnList(paramMap);
 		
-		return grimp.buildGrimp(grivo);
+		return grimp.buildGrimpToStr(grivo);
 	}
 }
